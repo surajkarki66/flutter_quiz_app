7d32a3c4734bc609d05b584657562197314b141a
surajkarki66 <suraj.karki500@protonmail.com>
2023-05-17 21:14:48 +0545

    finished start screen of the app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_quiz_app/start_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 80, 56, 122),
+            Color.fromARGB(255, 129, 71, 230),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: const StartScreen(),
+      ),
+    ),
+  ));
+}
